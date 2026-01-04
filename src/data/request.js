43d7acac1851fcd1add5df9abf212c5ac1f37db4
DavidZhang <3065983144@qq.com>
2026-01-04 11:02:53 +0800

    更新请求头中的授权令牌以保持安全性

--- src/data/request.js
+++ src/data/request.js
@@ -3,6 +3,6 @@ module.exports = axios.create({
 	baseURL: "https://gwowner.jiangongdata.com",
 	headers: {
 		authorization:
-			"JwtA eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpc3N1ZXIiLCJidXNpbmVzc0lkIjoiT1dORVIxMDg5NzY1MTM5MjI0MjE1NTUyIiwiaWF0IjoxNzY2MzczODczfQ.cOo3Nzq1sCw9SWHpJZU2XKG2Lv7EDeJIdNPuWJB9cEY",
+			"JwtA eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJpc3N1ZXIiLCJidXNpbmVzc0lkIjoiT1dORVIxMDg5NzY1MTM5MjI0MjE1NTUyIiwiaWF0IjoxNzY3NDk0NzM4fQ.uujLKuA1hNavk2UJUaCmHMFes5nXwY_WjUtqRV4pL8o",
 	},
 })
